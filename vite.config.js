967234292e0f6421b9eaa4f3558aa5475aac61fc
Daniel Morales <adanielmorales0@gmail.com>
2026-05-14 15:44:07 -0700

    construccion del about

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   base: '/tlalocan/',
+  build: {
+    outDir: 'docs',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+      },
+    },
+  },
 });
